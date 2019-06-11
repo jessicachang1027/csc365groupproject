da2bf9c38e7894df61e049d69e6e3a3d7b6bc10b
Adam Andersson <adam.p.andersson@gmail.com>
2019-06-10 18:15:32 -0700

    added checkActive and checkCardOwner triggers + updated checkCharge to check payments

--- src/chargeCardTrigger.sql
+++ src/chargeCardTrigger.sql
@@ -1,13 +1,18 @@
 drop trigger if exists chargeCard;
 drop trigger if exists checkCharge;
+drop trigger if exists checkActive;
+drop trigger if exists checkCardOwner;
 
 CREATE TRIGGER chargeCard
 AFTER INSERT ON Reservations
 FOR EACH ROW
    UPDATE CreditCard
-      SET currBalance = (DATEDIFF(toDate(NEW.CheckOut), toDate(NEW.CheckIn))
-                * NEW.rate) + currBalance
-      WHERE (SELECT username FROM Customers WHERE CreditCard.custId = username);
+      SET CreditCard.balance = (DATEDIFF(toDate(NEW.CheckOut), toDate(NEW.CheckIn))
+                * NEW.rate) + CreditCard.balance
+WHERE EXISTS (SELECT * FROM Payment, Customers 
+			   WHERE Payment.cardNum = CreditCard.cardNum
+			   and Payment.cID = Customers.username
+			   and NEW.code = Payment.resID);
       
 DELIMITER $    
 CREATE TRIGGER checkCharge
@@ -17,6 +22,34 @@ BEGIN
    IF NEW.balance >= NEW.creditLim THEN
       SIGNAL SQLSTATE '12345'
       SET MESSAGE_TEXT = 'check constraint on creditLimit failed';
+   END IF;
+END$
+DELIMITER ;
+
+DELIMITER $    
+CREATE TRIGGER checkActive
+BEFORE UPDATE ON CreditCard
+FOR EACH ROW
+BEGIN
+   IF !NEW.active THEN
+      SIGNAL SQLSTATE '12345'
+      SET MESSAGE_TEXT = 'check constraint on active failed';
+   END IF;
+END$
+DELIMITER ;
+
+DELIMITER $    
+CREATE TRIGGER checkCardOwner
+AFTER INSERT ON Reservations
+FOR EACH ROW
+BEGIN
+   IF !(EXISTS (SELECT * FROM Payment, Customers 
+			   WHERE Payment.cardNum = CreditCard.cardNum
+			   and Payment.cID = Customers.username
+			   and NEW.code = Payment.resID
+               and CreditCard.customerID = Customers.username)) THEN
+      SIGNAL SQLSTATE '12345'
+      SET MESSAGE_TEXT = 'check constraint on card ownership failed';
    END IF;
 END$
 DELIMITER ;
